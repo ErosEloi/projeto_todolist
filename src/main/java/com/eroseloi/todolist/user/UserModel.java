f4fbbb984f9833e1299eb0a8c1299a6a539dbb1a
Eros Eloi <eroseloi92@gmail.com>
2024-07-18 16:55:27 -0300

    Adição do lombok ao projeto

--- src/main/java/com/eroseloi/todolist/user/UserModel.java
+++ src/main/java/com/eroseloi/todolist/user/UserModel.java
@@ -1,5 +1,8 @@
 package com.eroseloi.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     
     //Atributos
@@ -8,33 +11,7 @@ public class UserModel {
     private int password;
 
 
-    // Get e Set - Username
-    public void setUsername(String username){
-        this.username = username;
-    }
 
-    public String getUsername() {
-        return username;
-    }
-
-    // Get e Set - Name
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    //Get e Set - Password
-    public void setPassword(int password) {
-        this.password = password;
-    }
-
-    public int getPassword() {
-        return password;
-    }
-
-
+   
 
 }
