eaf533567d60bdefc0af9e178940189ffe429ff9
Eros Eloi <eroseloi92@gmail.com>
2024-07-18 16:39:08 -0300

    Criação de Get e Set

--- src/main/java/com/eroseloi/todolist/user/UserModel.java
+++ src/main/java/com/eroseloi/todolist/user/UserModel.java
@@ -2,9 +2,39 @@ package com.eroseloi.todolist.user;
 
 public class UserModel {
     
-    public String username;
-    public String name;
-    public int password;
+    //Atributos
+    private String username;
+    private String name;
+    private int password;
+
+
+    // Get e Set - Username
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    // Get e Set - Name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //Get e Set - Password
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
 
 
 }
