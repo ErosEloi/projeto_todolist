0688d72b7cf9b99d164d8263b09ec25e3fb16c33
Eros Eloi <eroseloi92@gmail.com>
2024-07-19 14:37:46 -0300

    Criaçao da tabela usuarios

--- src/main/java/com/eroseloi/todolist/user/UserModel.java
+++ src/main/java/com/eroseloi/todolist/user/UserModel.java
@@ -1,16 +1,29 @@
 package com.eroseloi.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     
-    //Atributos
     private String username;
     private String name;
     private int password;
 
-
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
    
 
