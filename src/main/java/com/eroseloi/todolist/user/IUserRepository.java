04f9bae45e7408bf5aa830dfe94e9d4ee468a8fb
Eros Eloi <eroseloi92@gmail.com>
2024-07-19 15:51:23 -0300

    hash de senha

--- src/main/java/com/eroseloi/todolist/user/IUserRepository.java
+++ src/main/java/com/eroseloi/todolist/user/IUserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<UserModel, UUID>{
 
-
+    //Metodo para procurar por username - criado "manualmente"
+   UserModel findByUsername(String username);
     
 } 
