7aec3cd47427b77eb4b8d38661b725d96de23ebb
Eros Eloi <eroseloi92@gmail.com>
2024-07-22 20:20:44 -0300

    Validação de credenciais

---
+++ src/main/java/com/eroseloi/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,79 @@
+package com.eroseloi.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.eroseloi.todolist.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+
+        if (servletPath.equals("/tasks/")) {
+
+            // Pegar a autenticação (usuário e senha)
+
+            var authorization = request.getHeader("Authorization");
+
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            // Decodificação das credenciais
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecode);
+
+            // Separação das credenciais
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            // Teste para verificação
+            System.out.println("Authorization");
+            System.out.println(username);
+            System.out.println(password);
+
+            // Validar usuário
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+
+                // Validar sennha
+
+                // Comparação entre a senha recebida e a senha cadastrada no Banco de dados
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+                if (passwordVerify.verified) {
+                    chain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+
+                // Proseguir
+
+            }
+
+        } else {
+            chain.doFilter(request, response);
+        }
+
+    }
+
+}
