b94e4cc51212d805fc7e22cc42c5bc3fd2292a7f
Eros Eloi <eroseloi92@gmail.com>
2024-07-23 15:50:22 -0300

    Update de tarefa

--- src/main/java/com/eroseloi/todolist/task/ITaskRepository.java
+++ src/main/java/com/eroseloi/todolist/task/ITaskRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    //Filtro para listar as task por usuário
     List<TaskModel> findByIdUser(UUID idUser);
 }
--- src/main/java/com/eroseloi/todolist/task/TaskController.java
+++ src/main/java/com/eroseloi/todolist/task/TaskController.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,12 +52,21 @@ public class TaskController {
         return ResponseEntity.status(200).body(task);
     }
 
-    
+    // Listagem de task por usuário
     @GetMapping("/")
     public List<TaskModel> list(HttpServletRequest request){
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+        
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        taskModel.setId(id);
+        this.taskRepository.save(null);
     }
 
 
