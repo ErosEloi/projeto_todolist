d3b1e27282029bc54a081264968a8af52ef8ffb9
Eros Eloi <eroseloi92@gmail.com>
2024-07-23 14:53:01 -0300

    Validação de horas

--- src/main/java/com/eroseloi/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/eroseloi/todolist/filter/FilterTaskAuth.java
@@ -61,6 +61,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                 var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
                 if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
                     chain.doFilter(request, response);
                 } else {
                     response.sendError(401);
