a6423cf8ffe172c6b5895e9d3e93a58e99bc606e
Eros Eloi <eroseloi92@gmail.com>
2024-07-23 15:31:44 -0300

    Listando as tarefas

--- src/main/java/com/eroseloi/todolist/task/TaskController.java
+++ src/main/java/com/eroseloi/todolist/task/TaskController.java
@@ -1,12 +1,14 @@
 package com.eroseloi.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +28,35 @@ public class TaskController {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
 
+        // Validação de data para as tarefas
+
         var currentDate = LocalDateTime.now();
-        if (currentDate.isAfter(taskModel.getStartAt())) {
+        // Validação de data de inicio menor que data atual - Validação de data de fim é
+        // menor que data atual
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("A data de inicio deve ser maior que a data atual");
+                    .body("A data de inicio / termino deve ser maior que a data atual");
+
+        }
+
+        // Validação de data de incio menor que data de termino
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de inicio deve ser maior que a data de termino");
 
         }
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(200).body(task);
     }
+
+    
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+
 
 }
